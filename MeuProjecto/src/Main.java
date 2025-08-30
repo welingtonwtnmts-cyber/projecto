bfb3b27dff222961420ab1c9428abdec2a646f07
Welington <welingtonwtnmts@gmail.com>
2025-08-30 01:58:24 -0300

    outroProjeto

---
+++ MeuProjecto/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Well";
+        String cpf = "35464921885";
+        float valor = 2050;
+
+        System.out.printf("Dados do cliente: ");
+        System.out.println("Nome: " + nome + ", CPF: " + cpf + ", Valor: " + valor);
+
+
+    }
+}
