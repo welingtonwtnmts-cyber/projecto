d059e81f00926ed6192b22678d90fe7f634f939a
Welington <welingtonwtnmts@gmail.com>
2025-08-30 04:41:20 -0300

    Atualização

--- usu/src/Main.java
+++ usu/src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         String cpf = "000.000.000-00";
         String nome = "Champs";
         String endereco = " Rua: Jatobá" ;
@@ -13,6 +15,9 @@ public class Main {
 
         System.out.println("Dados Pessoais: " );
         System.out.println("Nome: " + nome + ", CPF: " + cpf + ", Endereço" + endereco + ", Número da residencia" + nr + "Conta" + valor );
+
+        String nom = scanner.nextLine();
+        System.out.println( "Olá, " + nom + "!");
 
 
 
