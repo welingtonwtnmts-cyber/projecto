7c1768af6ef5850157b5bae0ea5787d4f266146c
Welington <welingtonwtnmts@gmail.com>
2025-08-30 03:02:27 -0300

    Projeto atualizado

---
+++ usu/src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String cpf = "000.000.000-00";
+        String nome = "Champs";
+        String endereco = " Rua: Jatobá" ;
+        int nr = 77;
+        float valor = 2000 ;
+
+
+        System.out.println("Dados Pessoais: " );
+        System.out.println("Nome: " + nome + ", CPF: " + cpf + ", Endereço" + endereco + ", Número da residencia" + nr + "Conta" + valor );
+
+
+
+    }
+}
