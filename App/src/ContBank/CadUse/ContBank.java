47b317961531836c45d1e05c39cdb57f1f23ab5b
Welington <welingtonwtnmts@gmail.com>
2025-09-13 03:10:22 -0300

    cadusubank

---
+++ App/src/ContBank/CadUse/ContBank.java
@@ -0,0 +1,36 @@
+package ContBank.CadUse;
+
+
+public class ContBank {
+
+    private int agnc;    // agência
+    private int nmcont;  // número da conta
+    private float vlcont; // valor/saldo
+
+    // GETTERS (pegam o valor)
+    public int getAgencia() {
+        return agnc;
+    }
+
+    public int getConta() {
+        return nmcont;
+    }
+
+    public float getSaldo() {
+        return vlcont;
+    }
+
+    // SETTERS (definem o valor)
+    public void setAgencia(int agnc) {
+        this.agnc = agnc;
+    }
+
+    public void setConta(int nmcont) {
+        this.nmcont = nmcont;
+    }
+
+    public void setSaldo(float vlcont) {
+        this.vlcont = vlcont;
+    }
+}
+
